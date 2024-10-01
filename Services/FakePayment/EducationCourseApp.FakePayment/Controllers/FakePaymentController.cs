dfdfeddab3cde456bc9f5d71c534ca79c09ebb30
Zeynep <aydgms.zeynep@gmail.com>
2024-10-01 10:34:43 +0300

    fakePayment

---
+++ Services/FakePayment/EducationCourseApp.FakePayment/Controllers/FakePaymentController.cs
@@ -0,0 +1,15 @@
+﻿using EducationCourseApp.Shared.Controllers;
+using EducationCourseApp.Shared.Dtos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EducationCourseApp.FakePayment.Controllers;
+[Route("api/[controller]/[action]")]
+[ApiController]
+public class FakePaymentController : CustomBaseController
+{
+    [HttpPost]
+    public async Task<IActionResult> ReceivePayment()
+    {
+        return CreateActionResultInstance<NoContent>(Response<NoContent>.Success(200));
+    } 
+}
